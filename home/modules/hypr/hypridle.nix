532f6c649b812d38200f59ae57f38ef6fac6be55
JL <115737916+yikesboy@users.noreply.github.com>
2026-05-09 19:15:45 +0200

    ...

--- home/modules/hypr/hypridle.nix
+++ home/modules/hypr/hypridle.nix
@@ -1,48 +1,52 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ brightnessctl hyprlock ];
-  services.hypridle = {
-    enable = true;
+{ config, lib, pkgs, ... }:
+let cfg = config.my.hyprland;
+in {
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [ brightnessctl hyprlock ];
+    services.hypridle = {
+      enable = true;
 
-    settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          # dim backlight
+          {
+            timeout = 150;
+            on-timeout = "brightnessctl -s set 10";
+            on-resume = "brightnessctl -r";
+          }
+
+          # keyboard backlight
+          {
+            timeout = 150;
+            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+            on-resume = "brightnessctl -rd rgb:kbd_backlight";
+          }
+
+          # lockscreen
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+
+          # screen off after lockscreen
+          {
+            timeout = 330;
+            on-timeout = "on-timeout = hyprctl dispatch dpms off";
+            on-resume = "on-resume = hyprctl dispatch dpms on";
+          }
+
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
       };
-
-      listener = [
-        # dim backlight
-        {
-          timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
-        }
-
-        # keyboard backlight
-        {
-          timeout = 150;
-          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-          on-resume = "brightnessctl -rd rgb:kbd_backlight";
-        }
-
-        # lockscreen
-        {
-          timeout = 300;
-          on-timeout = "loginctl lock-session";
-        }
-
-        # screen off after lockscreen
-        {
-          timeout = 330;
-          on-timeout = "on-timeout = hyprctl dispatch dpms off";
-          on-resume = "on-resume = hyprctl dispatch dpms on";
-        }
-
-        {
-          timeout = 1800;
-          on-timeout = "systemctl suspend";
-        }
-      ];
     };
   };
 }
