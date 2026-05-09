40471895d3129dfb599e19f7e6e9c4cad412cf9d
JL <115737916+yikesboy@users.noreply.github.com>
2026-05-09 19:15:50 +0200

    ...

--- home/modules/hypr/hyprpaper.nix
+++ home/modules/hypr/hyprpaper.nix
@@ -1,16 +1,20 @@
-{ ... }: {
-  services.hyprpaper = {
-    enable = true;
+{ config, lib, ... }:
+let cfg = config.my.hyprland;
+in {
+  config = lib.mkIf cfg.enable {
+    services.hyprpaper = {
+      enable = true;
 
-    settings = {
-      ipc = "on";
-      preload = [ "/home/lukas/wallpapers/nixos.png" ];
+      settings = {
+        ipc = "on";
+        preload = [ "/home/lukas/wallpapers/nixos.png" ];
 
-      wallpaper = [
-        "eDP-1,/home/lukas/wallpapers/nixos.png"
-        "HDMI-A-1,/home/lukas/wallpapers/nixos.png"
-        "DP-2,/home/lukas/wallpapers/nixos.png"
-      ];
+        wallpaper = [
+          "eDP-1,/home/lukas/wallpapers/nixos.png"
+          "HDMI-A-1,/home/lukas/wallpapers/nixos.png"
+          "DP-2,/home/lukas/wallpapers/nixos.png"
+        ];
+      };
     };
   };
 }
